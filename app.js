c1a7b7a89f138d4ed91377cd5979be1f438294a6
Julian Diaz <torresanibal336@gmail.com>
2025-12-15 23:10:10 -0500

    Se realiza el eejrcicio 1

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log("Incio"); // Se indica el inicio del programa
+
+// Se detiene el programa durante 2 segundos
+setTimeout(() => {
+    console.log("Pasaron dos segundos");
+}, 2000);
+
+// Se indica el fin del programa
+console.log("Fin sincrono");
