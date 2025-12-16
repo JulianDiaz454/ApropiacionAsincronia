9e0f9dcb0ff2ceb9ace6924763dd56149019b033
Julian Diaz <torresanibal336@gmail.com>
2025-12-15 23:11:27 -0500

    Se realiza el ejercicio 2

---
+++ app2.js
@@ -0,0 +1,10 @@
+// Se indica el inicio del sistema
+console.log("Inicio");
+
+// Ciclo que bloqueara el sistema
+for (let i = 0; i < 90000000000000000; i++) {
+
+}
+
+// Este mensaje tardara varios segundos en mostrarse
+console.log("Final");
